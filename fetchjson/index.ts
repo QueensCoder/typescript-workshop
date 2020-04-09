e086520af784b5465593807bacd3a422f2748a68
Ozal Khan <ozalkhan196@gmail.com>
2020-04-09 13:56:56 -0400

    added type for log function

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,7 +2,20 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+type logTodoType = (id: number, title: string, completed: boolean) => void;
+
+const logTodo: logTodoType = (id, title, completed) => {
+  console.log(id, title, completed);
+};
+
 axios.get(url).then((response) => {
-  const { ID, Title, finished } = response.data.todo;
-  console.log(ID, Title, finished);
+  const todo = response.data as Todo;
+  const { id, title, completed } = todo;
+  logTodo(id, title, completed);
 });
